b1f20546e4093c80c96e2ddbb3dd8157b9614bb9
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-07-29 11:17:28 -0400

    fixing storage dir location in upload files page

--- pages/upload_your_documents.py
+++ pages/upload_your_documents.py
@@ -4,6 +4,7 @@ import streamlit as st
 import util.file_reader as fr
 import path
 import sys
+
 # need a file upload section and a section to show what files have been uploaded already.
 st.set_page_config(
     page_title='Upload Your Documents',
@@ -11,7 +12,7 @@ st.set_page_config(
     layout='wide',
     initial_sidebar_state='auto'
 )
-
+#
 # directory = path.Path(__file__).abspath()
 # sys.path.append(directory.parent.parent)
 
@@ -33,14 +34,12 @@ with col1:
                 file = fr.read_pdf(upload)
 
 with col2:
-    storage_dir = '**/stored_files'
+    storage_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '/stored_files')
     st.header('Your uploaded files will appear here')
     files = [f for f in os.listdir(storage_dir) if os.path.isfile(os.path.join(storage_dir, f))]
     if files is not None:
         for f in files:
             st.write(f)
-    else:
-        st.header("No files uploaded.")
             # download files button
             # with open(storage_dir + f, 'rb') as opened_file:
             #     st.download_button(label=f'Download File: {f}', file_name=f, data=opened_file)

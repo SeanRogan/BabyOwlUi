a7a7b8620d560769ad58122ead23c33dcf63c616
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-10-29 19:12:48 -0400

    new way of handling file for download.

--- constants/settings.py
+++ constants/settings.py
@@ -1,4 +1,4 @@
-SERPAPI_KEY = ""
+SERPAPI_KEY = "hjhjhkkhuhugf99uf3a45f30f820b9e05a22a55811ea2ce43c666c85b3178732d9c44c2ef37000a8"
 OPEN_AI_API_KEY = ''
 EMBEDDINGS_MODEL = "text-embedding-ada-002"
 COMPLETIONS_LLM_MODEL = "gpt-3.5-turbo-16k"
